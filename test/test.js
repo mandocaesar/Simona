b3883a9563ae11cc8750c2f195f8516eeea8daed
Armand Caesar <armand.caesar@gmail.com>
2017-03-25 18:18:46 +0700

    update package.json add test

---
+++ test/test.js
@@ -0,0 +1,18 @@
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var assert = require('assert');
+
+chai.use(chaiHttp);
+ describe('/ Root', function(){
+      it('it should return 200', function(done) {
+        chai.request("http://localhost:3000")
+            .get('/')
+            .end(function (err, res) {
+                res.should.have.status(200);
+              done();
+            });
+      });
+  });
+
