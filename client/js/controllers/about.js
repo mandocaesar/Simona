6a28dc4e9331791fece54cefb59d58fdce3d80c1
Armand Caesar <armand.caesar@gmail.com>
2017-03-27 15:56:21 +0700

    update about, underscore, and  capaian terbesar

---
+++ client/js/controllers/about.js
@@ -0,0 +1,8 @@
+﻿
+var controllerId = 'app.views.about';
+angular.module('app').controller(controllerId, [
+    '$scope', 'upload', '$http', function ($scope, upload, $http) {
+        var vm = this;
+    
+    }
+])
