12f68239519dd10e8de973eab8b055c82dda65e5
Armand Caesar <armand.caesar@gmail.com>
2017-04-01 19:52:36 +0700

    update  gague style to default injection

--- client/index.js
+++ client/index.js
@@ -4,70 +4,70 @@
         '$scope', '$rootScope', '$window', function ($scope, $rootScope, $window) {
             var vm = this;
             $rootScope.load = function () {
-                $window.onload = function () {
-                    // alert("click start fetch Data");
-                    for (var i = 0 ; i < Gauge.Collection.length ;i ++) {
-                        var config = Gauge.Collection[i].config;
-                        config.colors.plate = '#666';
-                        config.colors.majorTicks = 'rgba(192,192,192,0.3)';
-                        config.colors.minorTicks = 'rgba(192,192,192,0.3)';
-                        config.colors.numbers = '#fff';
-                        config.colors.units = '#fff';
+                // $window.onload = function () {
+                //     // alert("click start fetch Data");
+                //     for (var i = 0 ; i < Gauge.Collection.length ;i ++) {
+                //         var config = Gauge.Collection[i].config;
+                //         config.colors.plate = '#666';
+                //         config.colors.majorTicks = 'rgba(192,192,192,0.3)';
+                //         config.colors.minorTicks = 'rgba(192,192,192,0.3)';
+                //         config.colors.numbers = '#fff';
+                //         config.colors.units = '#fff';
 
-                        config.highlights = [
-                                {
-                                    from: 0,
-                                    to: 10,
-                                    color: '#E0330A'
-                                },
-                                {
-                                    from: 10,
-                                    to: 20,
-                                    color: '#D9400D'
-                                },
-                                {
-                                    from: 20,
-                                    to: 30,
-                                    color: '#D14C0F'
-                                },
-                                {
-                                    from: 30,
-                                    to: 40,
-                                    color: '#C95912'
-                                },
-                                {
-                                    from: 40,
-                                    to: 50,
-                                    color: '#C26614'
-                                }, {
-                                    from: 50,
-                                    to: 60,
-                                    color: '#BA7317 '
-                                },
-                                {
-                                    from: 60,
-                                    to: 70,
-                                    color: '#B2801A'
-                                }, {
-                                    from: 70,
-                                    to: 80,
-                                    color: '#AB8C1C'
-                                }, {
-                                    from: 80,
-                                    to: 90,
-                                    color: '#A3991F'
-                                }, {
-                                    from: 90,
-                                    to: 100,
-                                    color: '#9CA621'
-                                }]
+                //         config.highlights = [
+                //                 {
+                //                     from: 0,
+                //                     to: 10,
+                //                     color: '#E0330A'
+                //                 },
+                //                 {
+                //                     from: 10,
+                //                     to: 20,
+                //                     color: '#D9400D'
+                //                 },
+                //                 {
+                //                     from: 20,
+                //                     to: 30,
+                //                     color: '#D14C0F'
+                //                 },
+                //                 {
+                //                     from: 30,
+                //                     to: 40,
+                //                     color: '#C95912'
+                //                 },
+                //                 {
+                //                     from: 40,
+                //                     to: 50,
+                //                     color: '#C26614'
+                //                 }, {
+                //                     from: 50,
+                //                     to: 60,
+                //                     color: '#BA7317 '
+                //                 },
+                //                 {
+                //                     from: 60,
+                //                     to: 70,
+                //                     color: '#B2801A'
+                //                 }, {
+                //                     from: 70,
+                //                     to: 80,
+                //                     color: '#AB8C1C'
+                //                 }, {
+                //                     from: 80,
+                //                     to: 90,
+                //                     color: '#A3991F'
+                //                 }, {
+                //                     from: 90,
+                //                     to: 100,
+                //                     color: '#9CA621'
+                //                 }]
 
-                        //for (var i = 0; i < Gauge.Collection.length; i++) {
-                            Gauge.Collection[i].updateConfig(config);
-                          //  console.log(Gauge.Collection[i].config)
-                       // }
-                    }
-                };
+                //         //for (var i = 0; i < Gauge.Collection.length; i++) {
+                //             Gauge.Collection[i].updateConfig(config);
+                //           //  console.log(Gauge.Collection[i].config)
+                //        // }
+                //     }
+                // };
             }
         }]);
 })();
